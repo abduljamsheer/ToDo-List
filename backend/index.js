6f88e615b477c6f8c2cae925b97efb256fe9a403
abduljamsheer <abduljamsheerh7@gmail.com>
2025-10-18 16:25:09 +0530

    port changed

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const dotenv=require('dotenv').config()
 const express=require('express');
 const app=express()
-const port=8001;
+const port=process.env.PORT ||8001;
 const connection = require("./database/db.js");
 const cors = require("cors");
 app.use(cors({
